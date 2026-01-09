e968bc2632968c8b08ec7ca938e302708c8c3948
null68 <fariscomaga@gmail.com>
2026-01-09 23:21:19 +0100

    refactor: defaults

--- FarmGame/src/Graphics/Vertex.h
+++ FarmGame/src/Graphics/Vertex.h
@@ -9,8 +9,8 @@ namespace Engine {
 		glm::vec2 TexCoord;
 
 		Vertex(glm::vec3 position, glm::vec3 normal, glm::vec2 tex_coord) : Position(position), Normal(normal), TexCoord(tex_coord) {}
-		Vertex(glm::vec3 position, glm::vec3 normal) : Position(position), Normal(normal) {}
-		Vertex(glm::vec3 position) : Position(position) {}
+		Vertex(glm::vec3 position, glm::vec3 normal) : Position(position), Normal(normal), TexCoord(glm::vec2(0.0f)) {}
+		Vertex(glm::vec3 position) : Position(position), Normal(glm::vec3(0.0f)), TexCoord(glm::vec2(0.0f)) {}
 		Vertex() : Position(glm::vec3(0.0f)), Normal(glm::vec3(0.0f)), TexCoord(glm::vec2(0.0f)) {}
 	};
 }
