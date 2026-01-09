0533a475f376f3a17fe2212620f3718705696eb0
null68 <fariscomaga@gmail.com>
2026-01-09 23:20:45 +0100

    fix: multiple points per stage

--- FarmGame/src/Graphics/Shader/ShaderProgram.cpp
+++ FarmGame/src/Graphics/Shader/ShaderProgram.cpp
@@ -24,11 +24,11 @@ namespace Engine {
 			char infoLog[512];
 			glGetShaderInfoLog(m_VertexShaderId, 512, nullptr, infoLog);
 			throw std::runtime_error(std::string("Vertex shader compilation failed: ") + infoLog);
-		} // todo: ne znam da li je ovo najpametnije error handlovanje, istraziti
+		} 
 		glAttachShader(m_Id, m_VertexShaderId);
 	}
 	void ShaderProgram::AttachFragment(const char* fragmentSource) {
-		m_FragmentShaderId = glCreateShader(GL_VERTEX_SHADER);
+		m_FragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
 
 		int success;
 
@@ -41,7 +41,7 @@ namespace Engine {
 			char infoLog[512];
 			glGetShaderInfoLog(m_FragmentShaderId, 512, nullptr, infoLog);
 			throw std::runtime_error(std::string("Fragment shader compilation failed: ") + infoLog);
-		} // todo: ne znam da li je ovo najpametnije error handlovanje, istraziti
+		}
 		glAttachShader(m_Id, m_FragmentShaderId);
 	}
 	void ShaderProgram::Link() {
@@ -68,4 +68,50 @@ namespace Engine {
 	void ShaderProgram::Unbind() const {
 		glUseProgram(0);
 	}
+
+	int ShaderProgram::GetUniformLocation(const char* name) const {
+		auto it = m_UniformLocationCache.find(name);
+		if (it != m_UniformLocationCache.end())
+			return it->second;
+
+		int location = glGetUniformLocation(m_Id, name);
+
+		m_UniformLocationCache[name] = location;
+		return location;
+	}
+
+	void ShaderProgram::SetUniform1i(const char* name, int value) const {
+		glUniform1i(GetUniformLocation(name), value);
+	}
+
+	void ShaderProgram::SetUniform1f(const char* name, float value) const {
+		glUniform1f(GetUniformLocation(name), value);
+	}
+
+	void ShaderProgram::SetUniform1b(const char* name, bool value) const {
+		glUniform1i(GetUniformLocation(name), value ? 1 : 0);
+	}
+
+	void ShaderProgram::SetUniformVec2f(const char* name, const glm::vec2& vec) const {
+		glUniform2f(GetUniformLocation(name), vec.x, vec.y);
+	}
+
+	void ShaderProgram::SetUniformVec3f(const char* name, const glm::vec3& vec) const {
+		glUniform3f(GetUniformLocation(name), vec.x, vec.y, vec.z);
+	}
+
+	void ShaderProgram::SetUniformVec4f(const char* name, const glm::vec4& vec) const {
+		glUniform4f(GetUniformLocation(name), vec.x, vec.y, vec.z, vec.w);
+	}
+
+	void ShaderProgram::SetUniformMat4f(const char* name, const glm::mat4& mat) const {
+		glUniformMatrix4fv(
+			GetUniformLocation(name),
+			1,
+			GL_FALSE,
+			glm::value_ptr(mat)
+		);
+	}
+
 }
+	
--- FarmGame/src/Graphics/Shader/ShaderProgram.h
+++ FarmGame/src/Graphics/Shader/ShaderProgram.h
@@ -1,6 +1,9 @@
 #pragma once
 
-// TBA: glUniforms za shader variables, cacheovanje uniformi, unordered_map mozda
+#include <unordered_map>
+#include <string>
+#include <glm.hpp>
+#include <gtc/type_ptr.hpp>
 
 namespace Engine {
 	class ShaderProgram {
@@ -12,9 +15,19 @@ namespace Engine {
 		void Link();
 		void Bind() const;
 		void Unbind() const;
+		unsigned int getId() const { return m_Id; };
+		int GetUniformLocation(const char* name) const; 
+		void SetUniform1i(const char* name, int value) const;
+		void SetUniform1f(const char* name, float value) const;
+		void SetUniformVec2f(const char* name, const glm::vec2& vec) const;
+		void SetUniformVec3f(const char* name, const glm::vec3& vec) const;
+		void SetUniformVec4f(const char* name, const glm::vec4& vec) const;
+		void SetUniformMat4f(const char* name, const glm::mat4& mat) const;
+		void SetUniform1b(const char* name, bool value) const;
 	private:
 		unsigned int m_Id;
 		unsigned int m_VertexShaderId;
 		unsigned int m_FragmentShaderId;
+		mutable std::unordered_map<std::string, int> m_UniformLocationCache; // mutable jer se koristi u const funkcijama
 	};
 }
