091f4684f138d630b0895b05093610f4018ffde5
null68 <fariscomaga@gmail.com>
2026-09-26 22:17:07 +0200

    feat: load gltf model

--- FarmGame/src/Assets/AssetManager.cpp
+++ FarmGame/src/Assets/AssetManager.cpp
@@ -23,4 +23,30 @@ namespace Engine {
 		strcpy_s(shaderCodeCStr, shaderCode.size() + 1, shaderCode.c_str());
 		return shaderCodeCStr;
 	}
+
+	bool AssetManager::LoadGLTFModel(const std::string& filePath, tg3_model& model) {
+		tg3_parse_options opts;
+		tg3_error_stack errors;
+
+		tg3_parse_options_init(&opts);
+		tg3_error_stack_init(&errors);
+
+		tg3_error_code err = tg3_parse_file(&model, &errors, filePath.c_str(), filePath.size(), &opts);
+
+		if (err != TG3_OK) {
+			for (uint32_t i = 0; i < errors.count; i++) {
+				fprintf(stderr, "[%d] %s\n", (int)errors.entries[i].severity,
+					errors.entries[i].message ? errors.entries[i].message : "(null)");
+			}
+
+			tg3_error_stack_free(&errors);
+			tg3_model_free(&model);
+
+			return false;
+		}
+
+		tg3_error_stack_free(&errors);
+
+		return true;
+	}
 }
--- FarmGame/src/Assets/AssetManager.h
+++ FarmGame/src/Assets/AssetManager.h
@@ -1,10 +1,12 @@
 #pragma once
 
 #include <unordered_map>
+#include <tiny_gltf_v3.h>
 
 namespace Engine {
 		class AssetManager {
 	public:
 		static const char* LoadShaderFile(const char* filePath);
+		static bool LoadGLTFModel(const std::string& filePath, tg3_model& model);
 	};
 }
